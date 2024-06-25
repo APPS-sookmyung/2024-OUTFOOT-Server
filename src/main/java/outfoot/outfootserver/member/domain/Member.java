0af1b191c509f18333da908269bd53bf89292491
ljinny <leejieun2003@gmail.com>
2024-06-25 16:16:34 +0900

    Feat: member 설정

--- src/main/java/outfoot/outfootserver/member/domain/Member.java
+++ src/main/java/outfoot/outfootserver/member/domain/Member.java
@@ -6,7 +6,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import outfoot.outfootserver.common.BaseTimeEntity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity @Getter
 @Table(name = "member")
@@ -17,6 +22,9 @@ public class Member extends BaseTimeEntity {
     @Id @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "users_uuid", columnDefinition = "BINARY(16)", unique = true)
+    private UUID userId;
 
     @NotNull @Column(unique = true)
     private String username;
@@ -30,11 +38,25 @@ public class Member extends BaseTimeEntity {
     @NotNull
     private String password;
 
+    @Column(name = "provider", nullable = false, length = 10)
+    private String provider;
+
+    @Column(name = "provider_id", nullable = false, length = 50)
+    private String providerId;
+
 //    @NotNull
     @Column(unique = true)
     private String code;
 
     private String myIntro;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, length = 20)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", length = 20)
+    private LocalDateTime updatedAt;
 
     @Builder
     public Member(String username, String nickname, String email, String password) {
