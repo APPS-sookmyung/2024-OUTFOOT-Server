458f77a87157908284b55a4ad402afaf4e448e4b
ljinny <leejieun2003@gmail.com>
2024-06-25 16:22:30 +0900

    Feat: member repository 메소드 추가

--- src/main/java/outfoot/outfootserver/member/repository/MemberRepository.java
+++ src/main/java/outfoot/outfootserver/member/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package outfoot.outfootserver.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import outfoot.outfootserver.member.domain.Member;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.userId = :userId")
+    Optional<Member> findByUserId(UUID userId);
+
+    Member findByProviderId(String providerId);
 }
