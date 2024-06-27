2cab446ec73da5975f4bed5f8a1fddfb094975e5
ajeong <ajung7038@naver.com>
2024-06-28 01:16:14 +0900

    Feat: Swagger 설정 및 컨트롤러 애노테이션 추가 #7

--- src/main/java/outfoot/outfootserver/HealthCheck.java
+++ src/main/java/outfoot/outfootserver/HealthCheck.java
@@ -1,11 +1,13 @@
 package outfoot.outfootserver;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import outfoot.outfootserver.common.response.BasicResponse;
 import outfoot.outfootserver.common.response.ResponseUtil;
 
 @RestController
+@Tag(name = "Health Check", description = "API 테스트")
 public class HealthCheck {
 
     @GetMapping("/health")
--- src/main/java/outfoot/outfootserver/member/controller/MemberController.java
+++ src/main/java/outfoot/outfootserver/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package outfoot.outfootserver.member.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +14,21 @@ import outfoot.outfootserver.member.service.MemberService;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Tag(name = "회원가입", description = "Member API")
 public class MemberController {
-    private final MemberService memberService;
-
-    // 내 정보 확인
-    // BasicResponse로 감싸주기
     @PostMapping("/signup")
+    @Parameters({
+            @Parameter(name = "username", description = "공백 X", example = "ajeong7038"),
+            @Parameter(name = "nickname", description = "닉네임", example = "ajeong"),
+            @Parameter(name = "password", description = "공백 X", example = "password123"),
+            @Parameter(name = "email", description = "공백 X", example = "ajung7038@naver.com"),
+    })
     public BasicResponse<String> SignUp (@Valid@RequestBody SignUpRequest dto) {
         long memberId = memberService.save(dto);
         return ResponseUtil.success("가입 성공 " + memberId);
     }
+
+    private final MemberService memberService;
+    // 내 정보 확인
+    // BasicResponse로 감싸주기
 }
