b481ce19966f2a3d7df0f86c2513eb26a5ef4305
ljinny <leejieun2003@gmail.com>
2024-07-07 22:04:49 +0900

    Feat: 네이버 유저 정보 DTO

---
+++ src/main/java/outfoot/outfootserver/auth/dto/NaverUserInfo.java
@@ -0,0 +1,22 @@
+package outfoot.outfootserver.auth.dto;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+
+    private Map<String, Object> attributes;
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
