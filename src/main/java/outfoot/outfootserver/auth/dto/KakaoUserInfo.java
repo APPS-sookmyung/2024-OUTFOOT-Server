a78d36b60040c9e11ad25e0ab7665bf59b6ed445
ljinny <leejieun2003@gmail.com>
2024-07-07 22:01:25 +0900

    Feat: 카카오 유저 정보 DTO

---
+++ src/main/java/outfoot/outfootserver/auth/dto/KakaoUserInfo.java
@@ -0,0 +1,25 @@
+package outfoot.outfootserver.auth.dto;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo{
+
+    private Map<String, Object> attributes;
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getName() {
+        return (String) ((Map) attributes.get("properties")).get("nickname");
+    }
+}
